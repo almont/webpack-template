3715760d61bf68a4519e5ede9be59fb7b13fa090
André Luiz Monteiro de Castro <andre.monteiro@rappbrasil.com.br>
2018-07-31 10:47:18 -0300

    Melhorias

--- js/app.js
+++ js/app.js
@@ -7,23 +7,23 @@ const subjectElement = document.getElementById('subject');
 const charsCounterElement = document.getElementById('chars-counter');
 const copyElement = document.getElementById('copy');
 const emojiElement = document.getElementById('emoji');
-
+let IS_OK = false;
 
 // Array of bad words
 const badWords = ['$', 'ganhe dinheiro enviando emails', 'trabalhe em casa', 'renda extra', 'crédito', 'seja seu próprio patrão', 'aplique agora', 'taxas baixas', 'dobre sua renda', 'ganhe', 'pago semanalmente', 'pagamento imediato', 'dinheiro rápido', 'liberdade financeira', 'saia da dívida', 'elimine a dívida', 'reduza suas taxas', 'refinancie', 'bônus', 'livre', 'empréstimo', '100%', 'emagreça', 'dieta', 'por que não consigo emagrecer', 'perca peso', 'perder gordura', 'fique sarado', 'secar barriga', 'compre', 'aproveite', 'grátis', 'desconto', 'promoção', 'oferta', 'imperdível', 'preços', 'valores', 'frete grátis', 'despachamos para todo o brasil', 'vagas abertas', 'telemarketing', 'curso', 'curso online', 'oportunidade', 'agência de modelos', 'oferta de emprego', 'parceria', 'lista de emails', 'lista de contatos', 'divulgue seu', 'divulgue sua', 'marketing por e-mail', 'sites de busca', 'clique', 'assine', 'visite o site', 'saiba mais', 'dê uma olhada', 'leia agora', 'não exclua', 'veja isso', 'confira', 'abra', 'isso não é spam', 'atenção', 'especialmente para você', 'a informação que você pediu', 'agora', 'já', 'última chance', 'hoje', 'tempo limitado', 'não perca tempo', '24 horas', 'urgente', 'imediato', 'só até amanhã', 'dinheiro', 'dinheiro a sério', 'vencedor', 'gratuito', 'Clique aqui', 'Anuncie', 'Ganhe dinheiro enviando e-mails', 'Lista de e-mails', 'Mala direta', 'Perda de peso', 'oi', 'urgentemente', 'amigo', 'escondido', 'para você', 'STOP', 'fora', 'oferecer', 'quente', 'incrível', 'satisfação', 'agir agora', 'aplicar agora', 'agora só', 'como se vê', 'como se vê na TV', 'evitar', 'ser seu próprio patrão', 'trabalhar em casa', 'em casa', 'comprar', 'ligue agora', 'bônus em dinheiro', 'o caixa livre', 'dobrar sua renda', 'ganhar', 'marketing multi nível', 'ganhar dinheiro', 'o pagamento imediato', 'o pagamento atrasado', 'gestão de fundos', 'de acesso gratuito', 'dom gratuito', 'informação livre', 'a oferta livre', 'medicina', 'soft tabs', 'cialis', 'xanax', 'valium', 'vicodin', 'Viagra', 'Levitra', 'plantas medicinais', 'alargamento', 'aberto agora', 'recolher', 'comparar', 'consolidar', 'débito', 'saia dívida', 'eliminar a dívida', 'reduzir sua taxa de hipoteca', 'refinanciar', 'as taxas mais baixas de seguros', 'seguro de vida', 'empréstimos', 'caro amigo', 'perder peso', 'grau em linha', 'marketing online', 'farmácia on-line', 'prometeu que', 'adolescente', 'vencedor ', 'você é um vencedor', 'sua família', 'seu won e-mail', 'por favor me ajude', 'o pedido de parceria', 'Deus te abençoe', 'comprar agora', 'destinatário desconhecido', 'Saldo em aberto', 'Fatura', 'Fatura de Cartão de crédito', 'Cobrança', 'Mensagem de Saldo em aberto'];
-
 
 // Array of emoji
 const emojiList = ['❤', '❥', '웃', '유', '🍾', '☮', '✌', '☏', '☢', '☠', '✔', '☑', '♚', '▲', '♪', '✈', '⌚', '¿', '♥', '❣', '♂', '♀', '⚤', 'Ⓐ', '✍', '✉', '☣', '☤', '✘', '☒', '♛', '▼', '♫', '⌘', '⌛', '¡', '♡', 'ღ', 'ツ', '☼', '☁', '❅', '♒', '✎', '©', '®', '™', 'Σ', '✪', '✯', '☭', '➳', '⚑', '✞', '℃', '℉', '°', '✿', 'ϟ', '☃', '☂', '✄', '¢', '€', '£', '∞', '✫', '★', '½', '☯', '✡', '☪', '😍', '😘', '😛', '😃', '😂', '😊', '😉', '😁', '😭', '😒', '😏', '😥', '😳', '😲', '😯', '😱', '😰', '😓', '👿', '💤', '💩', '👏', '✌', '☺', '👌', '👍', '💪', '👊', '👉', '✊', '🙈', '🙊', '🙉', '🎁', '🎉', '➡', '✅', '🆗', '✔', '🎶', '🎵', '🎧', '🔴', '🔵', '⚫', '💰', '💸', '💲', '☀', '☁', '🔥', '☕', '☔', '❄', '👓', '🎓', '💍', '🚀'];
 
 
+
 // Build bad words and emoji lists
 (function () {
     // Words
-    listOfWordsElement.innerHTML = badWords.map((word) => `<li>${word}</li>`).join('');
+    listOfWordsElement.innerHTML = badWords.map((_word) => `<li>${_word}</li>`).join('');
     
     // Emoji
-    emojiListElement.innerHTML = emojiList.map((emoji) => `<li><a class="emoji-link" href="#" onclick="emojiSelect(event)">${emoji}</a></li>`).join('');
+    emojiListElement.innerHTML = emojiList.map((_emoji) => `<li><a class="emoji-link" href="#" onclick="emojiSelect(event)">${_emoji}</a></li>`).join('');
 })();
 
 
@@ -51,8 +51,9 @@ function showHide(_element, _status) {
 
 
 // Search for the bad words into subject line
-function matchWords(_subject, _words) {
-    let regex = new RegExp("(?:" + _words.join("|") + ")", "gi");
+function matchWords(_subject) {
+    const words = badWords.map((_word) => _word.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&')).join('|');
+    const regex = new RegExp("(?:" + words + ")", "gi");
     
     return _subject.match(regex) || [];
 }
@@ -60,29 +61,50 @@ function matchWords(_subject, _words) {
 
 // Highlight words inside the subject
 function highlightWords(_subject, _result) {
-    let subject = _subject;
-    let result = _result.map((word) => word.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&')).join('|');
+    const subject = _subject;
+    const result = _result.map((_word) => _word.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&')).join('|');
 
-    return subject.replace(RegExp(result, "gi"), (word) => `<span class='bg-danger text-warning'>${word}</span>`);
+    return subject.replace(RegExp(result, "gi"), (_word) => `<span class='bg-danger text-warning'>${_word}</span>`);
 }
 
 
-// Submit
-function search(e) {
-    e.preventDefault();
+// Show the subject validation result
+function showResult(_subject, _result) {
+    if (_result.length == 0) {
+        IS_OK = true;
 
-    let subject = subjectElement.value;
-    let words = badWords.map((word) => word.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&'));
-    let result = matchWords(subject, words);
+        resultElement.classList.add('result-green');
+        resultElement.classList.remove('result-red');
 
-    resultElement.innerHTML = highlightWords(subject, result);
+    } else {
+        IS_OK = false;
+
+        resultElement.classList.add('result-red');
+        resultElement.classList.remove('result-green');
+    }
+
+    resultElement.innerHTML = highlightWords(_subject, _result);
     showHide(resultElement, 'show');
     showHide(subjectCopyElement, 'hide');
 }
 
 
+// Submit
+function search(_e) {
+
+    console.log('-> ', IS_OK, _e)
+
+    _e.preventDefault();
+
+    const subject = subjectElement.value;
+    const result = matchWords(subject);
+
+    showResult(subject, result);
+}
+
+
 // Result
-function result(e) {
+function result() {
     subjectElement.select();
 
     showHide(resultElement, 'hide');
@@ -91,7 +113,9 @@ function result(e) {
 
 // Char counter
 function charCounter() {
-    let chars = subjectElement.value;
+    const chars = subjectElement.value;
+    
+    search(event);
     
     if (chars.length > 60) {
         charsCounterElement.innerHTML = `<span class="chars-counter-red">${chars.length} caracteres</span>`;
@@ -99,7 +123,12 @@ function charCounter() {
         
     } else {
         charsCounterElement.innerHTML = `<span class="chars-counter-green">${chars.length} caracteres</span>`;
-        showHide(copyElement, 'show');
+        
+        if (IS_OK == true) {
+            showHide(copyElement, 'show');
+        } else {
+            showHide(copyElement, 'hide');
+        }
     }
 
     showHide(subjectCopyElement, 'hide');
@@ -109,197 +138,25 @@ subjectElement.addEventListener('input', charCounter);
 
 
 // Emoji list
-function showEmojiList(e) {
-    e.preventDefault();
+function showEmojiList(_e) {
+    _e.preventDefault();
 
     showHide(emojiElement);
 }
 
 
 // Emoji selector
-function emojiSelect(e) {
-    subjectElement.value += e.target.innerText;
+function emojiSelect(_e) {
+    subjectElement.value += _e.target.innerText;
     charCounter();
 }
 
 
 // Copy to clipboard
-function copySubject(e) {
+function copySubject() {
     subjectElement.select();
     document.execCommand('copy');
 
     showHide(subjectCopyElement, 'show');
     showHide(resultElement, 'hide');
 }
-
-
-/** BAD WORDS
-
-ganhe dinheiro enviando emails
-trabalhe em casa
-renda extra
-crédito
-seja seu próprio patrão
-aplique agora
-taxas baixas
-dobre sua renda
-ganhe
-pago semanalmente
-pagamento imediato
-dinheiro rápido
-liberdade financeira
-saia da dívida
-elimine a dívida
-reduza suas taxas
-refinancie
-bônus
-livre
-empréstimo
-100%
-emagreça
-dieta
-por que não consigo emagrecer
-perca peso
-perder gordura
-fique sarado
-secar barriga
-compre
-aproveite
-grátis
-desconto
-promoção
-oferta
-imperdível
-preços
-valores
-frete grátis
-despachamos para todo o brasil
-vagas abertas
-telemarketing
-curso
-curso online
-oportunidade
-agência de modelos
-oferta de emprego
-parceria
-lista de emails
-lista de contatos
-divulgue seu
-divulgue sua
-marketing por e-mail
-sites de busca
-clique
-assine
-visite o site
-saiba mais
-dê uma olhada
-leia agora
-não exclua
-veja isso
-confira
-abra
-isso não é spam
-atenção
-especialmente para você
-a informação que você pediu
-agora
-já
-última chance
-hoje
-tempo limitado
-não perca tempo
-24 horas
-urgente
-imediato
-só até amanhã
-dinheiro
-dinheiro a sério
-$
-vencedor
-gratuito
-Clique aqui
-Anuncie
-Ganhe dinheiro enviando e-mails
-Lista de e-mails
-Mala direta
-Perda de peso
-oi
-urgentemente
-amigo
-escondido
-para você
-STOP
-fora
-oferecer
-quente
-incrível
-satisfação
-agir agora
-aplicar agora
-agora só
-como se vê
-como se vê na TV
-evitar
-ser seu próprio patrão
-trabalhar em casa
-em casa
-comprar
-ligue agora
-bônus em dinheiro
-o caixa livre
-dobrar sua renda
-ganhar
-marketing multi nível
-ganhar dinheiro
-o pagamento imediato
-o pagamento atrasado
-gestão de fundos
-de acesso gratuito
-dom gratuito
-informação livre
-a oferta livre
-medicina
-soft tabs
-cialis
-xanax
-valium
-vicodin
-Viagra
-Levitra
-plantas medicinais
-alargamento
-aberto agora
-recolher
-comparar
-consolidar
-débito
-saia dívida
-eliminar a dívida
-reduzir sua taxa de hipoteca
-refinanciar
-as taxas mais baixas de seguros
-seguro de vida
-empréstimos
-caro amigo
-perder peso
-grau em linha
-marketing online
-farmácia on-line
-prometeu que
-adolescente
-vencedor
-você é um vencedor
-sua família
-seu won e-mail
-por favor me ajude
-o pedido de parceria
-Deus te abençoe
-comprar agora
-destinatário desconhecido
-Saldo em aberto
-Fatura
-Fatura de Cartão de crédito
-Cobrança
-Mensagem de Saldo em aberto
-
- */
