2b51c8b6a06eba6ea60918c087c7d84d039ad5c2
Andre Monteiro <andre.monteiro@proximitybr.com>
2019-12-03 17:26:08 -0300

    SASS improvement

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,29 @@
-import * as common from './common.js'
-import {Animal} from './animal.js'
-import {Cat} from './cat.js'
-import '../styles/index.scss'
+import * as common from './common.js';
+import { Animal } from './animal.js';
+import { Cat } from './cat.js';
+import '../styles/index.scss';
 
-
+const test = 'car';
 
 (() => {
+    let animal = new Animal('animal', 4);
+    common.log(animal);
+    //
+    console.log(animal.legs, 'info');
+    common.log(animal.legs, 'info');
+    animal.legs = 3;
+    common.log(animal.legs, 'info');
+    //
+    animal.makeNoise();
+    animal.makeNoise('Awwwww!');
+    //
+    common.log(animal.metaData, 'success');
+    //
+    let cat = new Cat('siamese', 4, true);
+    cat.makeNoise('Meow');
+    common.log(cat.metaData, 'warning');
 
-    let animal = new Animal('animal', 4)
-    common.log(animal)
-    //
-    console.log(animal.legs, 'info')
-    common.log(animal.legs, 'info')
-    animal.legs = 3
-    common.log(animal.legs, 'info')
-    //
-    animal.makeNoise()
-    animal.makeNoise('Awwwww!')
-    //
-    common.log(animal.metaData, 'success')
-    //
-    let cat = new Cat('siamese', 4, true)
-    cat.makeNoise('Meow')
-    common.log(cat.metaData, 'warning')
-
-
-    common.log('END', 'error')
+    common.log('END', 'error');
 
     // showOutline()
-    
-})()
+})();
